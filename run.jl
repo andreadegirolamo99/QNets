7560412e42f71d13a84a4da6c4d90617027dedf0
andreadegirolamo99 <andrea.degirolamo99@gmail.com>
2024-10-16 13:07:34 +0200

    Ready for first batch of simulations

--- run.jl
+++ run.jl
@@ -1,123 +1,105 @@
 include("src/percolation.jl")
 
 
-function run_mean(n, topology)
-    if !isdir("out_files/$(topology)_$n")
-        mkdir("out_files/$(topology)_$n")
+function run_mean(n, i, λmean, topology)
+    g = eval(Symbol(topology))(n, λ=λmean)
+
+    pairs = pairs_by_distance(g)
+
+    println("Mean of links: $λmean")
+    flush(stdout)
+    mean_ent = Dict()
+    mean_N = Dict()
+
+    for distance in sort(collect(keys(pairs)))[2:end]
+        ent = fill(0.0, length(pairs[distance]))
+        Ns = fill(0.0, length(pairs[distance]))
+        println("Computing entanglement for nodes at distance $(distance-1), λ = $λmean")
+        flush(stdout)
+
+        Threads.@threads for j in 1:length(pairs[distance])
+        # for j in 1:length(pairs[distance])
+            v1, v2 = pairs[distance][j]
+            sol = find_path(g, v1, v2, samples=600, σ=0.5, progressbar=false)
+            if !isnothing(sol)
+                path, λ, N = sol
+                ent[j] = 2*(1 - λ)
+                Ns[j] = N
+            end
+        end
+        println()
+        flush(stdout)
+
+        mean_ent[distance] = mean(ent)
+        mean_N[distance] = mean(Ns)
     end
-    if !isdir("out_files/$(topology)_$n/mean")
-        mkdir("out_files/$(topology)_$n/mean")
+
+    file = open("out_files/$(topology)_$n/mean_/$(i)_$(λmean).out", "w")
+    for dist in sort(collect(keys(mean_ent)))
+        write(file, "$(dist-1) $(mean_ent[dist]) $(mean_N[dist]) \n")
     end
+
+    flush(file)
+    close(file)
+end
+
+function run_dev(n, i, λmean, σ, topology, sample)
+    g = eval(Symbol(topology))(n, σ=σ, λmean=λmean)
+
+    pairs = pairs_by_distance(g)
+
+    @assert isapprox(mean(get_weights(g, pair...) for pair in pairs[1]), λmean)
+
+    println("Mean of links: $λmean")
+    flush(stdout)
+    mean_ent = Dict()
+    mean_N = Dict()
     
-    λs = collect(range(0.5, stop=1.0, length=101))
-
-    for (i, λmean) in enumerate(λs)
-        g = eval(Symbol(topology))(n, λ=λmean)
-
-        pairs = pairs_by_distance(g)
-
-        println("Mean of links: $λmean")
+    for distance in sort(collect(keys(pairs)))[2:end]
+        ent = fill(0.0, length(pairs[distance]))
+        Ns = fill(0.0, length(pairs[distance]))
+        println("Computing entanglement for nodes at distance $(distance-1), λ = $λmean")
         flush(stdout)
-        mean_ent = Dict()
-        mean_N = Dict()
 
-        for distance in sort(collect(keys(pairs)))[2:end]
-            ent = fill(0.0, length(pairs[distance]))
-            Ns = fill(0.0, length(pairs[distance]))
-            println("Computing entanglement for nodes at distance $(distance-1), λ = $λmean")
-            flush(stdout)
-
-            Threads.@threads for j in 1:length(pairs[distance])
-            # for j in 1:length(pairs[distance])
-                v1, v2 = pairs[distance][j]
-                sol = find_path(g, v1, v2, samples=300, σ=0.15, progressbar=false)
-                if !isnothing(sol)
-                    path, λ, N = sol
-                    ent[j] = 2*(1 - λ)
-                    Ns[j] = N
-                end
+        Threads.@threads for j in 1:length(pairs[distance])
+            v1, v2 = pairs[distance][j]
+            sol = find_path(g, v1, v2, samples=600, σ=0.5, progressbar=false)
+            if !isnothing(sol)
+                path, λ, N = sol
+                ent[j] = 2*(1 - λ)
+                Ns[j] = N
             end
-            println()
-            flush(stdout)
+        end
+        println()
+        flush(stdout)
 
-            mean_ent[distance] = mean(ent)
-            mean_N[distance] = mean(Ns)
-        end
-        file = open("out_files/$(topology)_$n/mean/$(i)_$(λmean).out", "w")
-        for dist in sort(collect(keys(mean_ent)))
-            write(file, "$(dist-1) $(mean_ent[dist]) $(mean_N[dist]) \n")
-        end
-        flush(file)
-        close(file)
+        mean_ent[distance] = mean(ent)
+        mean_N[distance] = mean(Ns)
     end
+
+    file = open("out_files/$(replace(topology, r"noisy_" => ""))_$n/std_dev=$(σ)/$(sample)/$(i)_$(λmean).out", "w")
+    for dist in sort(collect(keys(mean_ent)))
+        write(file, "$(dist-1) $(mean_ent[dist]) $(mean_N[dist]) \n")
+    end
+    flush(file)
+    close(file)
 end
 
-function run_dev(n, σ, topology)
-    if !isdir("out_files/$(replace(topology, r"noisy_" => ""))_$n")
-        mkdir("out_files/$(replace(topology, r"noisy_" => ""))_$n")
-    end
-    if !isdir("out_files/$(replace(topology, r"noisy_" => ""))_$n/std_dev=$(σ)")
-        mkdir("out_files/$(replace(topology, r"noisy_" => ""))_$n/std_dev=$(σ)")
-    end
-    s_num = isempty(readdir("out_files/$(replace(topology, r"noisy_" => ""))_$n/std_dev=$(σ)")) ? 1 : (maximum([parse(Int64, sample) for sample in readdir("out_files/$(topology)_$n/std_dev=$(σ)")]) + 1)
-    mkdir("out_files/$(replace(topology, r"noisy_" => ""))_$n/std_dev=$(σ)/$(s_num)")
-
-    λs = collect(range(0.5, stop=1.0, length=101))
-    for (i, λmean) in enumerate(λs)
-        g = eval(Symbol(topology))(n, σ=σ, λmean=λmean)
-
-        pairs = pairs_by_distance(g)
-
-        @assert isapprox(mean(get_weights(g, pair...) for pair in pairs[1]), λmean)
-
-        println("Mean of links: $λmean")
-        flush(stdout)
-        mean_ent = Dict()
-        mean_N = Dict()
-        
-        for distance in sort(collect(keys(pairs)))[2:end]
-            ent = fill(0.0, length(pairs[distance]))
-            Ns = fill(0.0, length(pairs[distance]))
-            println("Computing entanglement for nodes at distance $(distance-1), λ = $λmean")
-            flush(stdout)
-
-            Threads.@threads for j in 1:length(pairs[distance])
-                v1, v2 = pairs[distance][j]
-                sol = find_path(g, v1, v2, samples=300, σ=0.15, progressbar=false)
-                if !isnothing(sol)
-                    path, λ, N = sol
-                    ent[j] = 2*(1 - λ)
-                    Ns[j] = N
-                end
-            end
-            println()
-            flush(stdout)
-
-            mean_ent[distance] = mean(ent)
-            mean_N[distance] = mean(Ns)
-        end
-
-        file = open("out_files/$(replace(topology, r"noisy_" => ""))_$n/std_dev=$(σ)/$(s_num)/$(i)_$(λmean).out", "w")
-        for dist in sort(collect(keys(mean_ent)))
-            write(file, "$(dist-1) $(mean_ent[dist]) $(mean_N[dist]) \n")
-        end
-        flush(file)
-        close(file)
-    end
-end
-
-function run(N, topology, std_dev)
+function run(topology, N, i, λmean, std_dev, sample)
     if std_dev == 0
-        run_mean(N, topology)
+        run_mean(N, i, λmean, topology)
     else
-        run_dev(N, std_dev, topology)
+        run_dev(N, i, λmean, std_dev, topology, sample)
     end
 end
 
 topology = ARGS[1]
 N = parse(Int64, ARGS[2])
-std_dev = parse(Float64, ARGS[3])
+i = parse(Int64, ARGS[3])
+λmean = parse(Float64, ARGS[4])
+std_dev = parse(Float64, ARGS[5])
+sample = parse(Int64, ARGS[6])
 
-run(N, topology, std_dev)
+run(topology, N, i, λmean, std_dev, sample)
 
 # run_dev(6, 1, 0.01)
--- scheduler.py
+++ scheduler.py
@@ -1,11 +1,39 @@
 import os
+import numpy as np
 
 N = 10
-topologies = ["square_lattice", "diagonal_square_lattice", "honeycomb", "fully_connected_honeycomb"]
-sigmas = [0, 0.01, 0.07]
+topologies = ["square_lattice"]#["square_lattice", "diagonal_square_lattice", "honeycomb", "fully_connected_honeycomb"]
+sigmas = [0]#[0, 0.01, 0.07]
+lambdas = np.arange(0.5, 1.005, 1/200)
+n_samples = 1
+samples = []
 
 for topology in topologies:
+    if not os.path.isdir("out_files/{}_{}".format(topology, N)):
+        os.mkdir("out_files/{}_{}".format(topology, N))
+
     for sigma in sigmas:
         if sigma == 0:
-            topology = "noisy_" + topology
-        os.system('sbatch ./qnets.sh ' + topology + ' ' + str(N) + ' ' + str(sigma))
+            if not os.path.isdir("out_files/{}_{}/mean".format(topology, N)):
+                os.mkdir("out_files/{}_{}/mean".format(topology, N))
+        else:
+            if not os.path.isdir("out_files/{}_{}/std_dev={}".format(topology, N, sigma)):
+                os.mkdir("out_files/{}_{}/std_dev={}".format(topology, N, sigma))
+
+            s_num = 1 if not os.listdir("out_files/{}_{}/std_dev={}".format(topology, N, sigma)) else max([int(sample) for sample in os.listdir("out_files/{}_{}/std_dev={}".format(topology, N, sigma))]) + 1
+            for sample in range(n_samples):
+                os.mkdir("out_files/{}_{}/std_dev={}/{}".format(topology, N, sigma, s_num))
+                samples.append(s_num)
+                s_num += 1
+
+
+
+for topology in topologies:
+    for (i, lambda_) in enumerate(lambdas):
+        for sigma in sigmas:
+            if sigma > 0:
+                topology_ = "noisy_" + topology
+                for sample in samples:
+                    os.system('./qnets.sh ' + topology_ + ' ' + str(N) + ' ' + str(i+1) + ' ' + str(lambda_) + ' ' + str(sigma) + ' ' + str(sample))
+            else:
+                os.system('./qnets.sh ' + topology + ' ' + str(N) + ' ' + str(i+1) + ' ' + str(lambda_) + ' ' + str(sigma) + ' ' + str(0))
